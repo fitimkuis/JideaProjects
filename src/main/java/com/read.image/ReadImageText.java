948672194e4c1f72433343cbd260354a3feb57c7
fitim <fitimkuis@gmail.com>
2020-05-14 23:08:13 +0300

    tesse fix

--- src/main/java/com/read.image/ReadImageText.java
+++ src/main/java/com/read.image/ReadImageText.java
@@ -5,7 +5,23 @@ import net.sourceforge.tess4j.*;
 
 public class ReadImageText {
 
+    static Tesseract tesseract = new Tesseract();
+
     public static void main(String[] args) throws TesseractException {
+
+        try {
+
+            File image = new File("src\\main\\images\\image.png");
+            // the path of your tess data folder
+            // inside the extracted file
+            tesseract.setDatapath("src\\main\\tessdata");
+            // path of your image file
+            String text = tesseract.doOCR(image);
+            System.out.print(text);
+        }
+        catch (TesseractException e) {
+            e.printStackTrace();
+        }
 
         ReadText();
 
@@ -13,9 +29,9 @@ public class ReadImageText {
 
     public static void ReadText() throws TesseractException {
 
-        File image = new File("C:\\Users\\fitim\\IdeaProjects\\JideaProjects\\src\\main\\java\\com\\read.image\\eurotext.png");
-        Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath("C:/Users/fitim/IdeaProjects/JideaProjects/src/main/tessdata");
+        File image = new File("src\\main\\images\\eurotext.png");
+
+        tesseract.setDatapath("src\\main\\tessdata");
         tesseract.setLanguage("eng");
         tesseract.setPageSegMode(1);
         tesseract.setOcrEngineMode(1);
